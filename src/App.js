167d206fafa55af78b4fbd08b960dee1e168b9d6
Cris <duytruong0702@gmail.com>
2018-12-01 15:16:53 +0700

    Duy - Add router, Change layout navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import Header from './components/header';
+
+import {BrowserRouter ,Route, Switch} from 'react-router-dom'
+
+import Navbar from "./components/Navbar/Navbar"
 import Profile from './components/profile';
 import Newfeed from './components/newfeed';
 import People from './components/people';
-import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header/>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-3">
-              <Profile/>
-            </div>
-            <div class="col-sm-6">
-              <Newfeed/>
-            </div>
-            <div class="col-sm-3">
-             <People/>
-            </div>
-          </div>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <div className = "App">
+          <Navbar/>
+          <Switch>
+              <Route exact path="/" component={Profile} />
+          </Switch>
         </div>
-      </div>
+        
+      </BrowserRouter>
     );
   }
 }
--- src/components/newfeed.js
+++ src/components/newfeed.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import '../App.css';
+
 
 class Newfeed extends Component {
   render() {
