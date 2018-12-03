02086cf210f4764ed51f00d1c53afd03bd18e211
Cris <duytruong0702@gmail.com>
2018-12-03 12:16:18 +0700

    Duy - UPdate structure 2

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react';
 
 import {BrowserRouter ,Route, Switch} from 'react-router-dom'
 
-import Navbar from "./components/Layout/NavBar/Navbar"
+import Navbar from "./components/Layout/NavBar/HeaderBar/HeaderBar"
 import Profile from './components/Layout/LeftBar/profile';
 import Newfeed from './components/Newsfeed/newfeed';
 import People from './components/Layout/RightBar/people';
-
+import {BrowserRouter ,Route, Switch} from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
-        <div class="container">
-          <div class="row">
-            <Profile/>
-            
-            <Newfeed/>
-           
-            <People/> 
-          </div>
-        </div>
+        <div>
+          <Navbar/>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
+              <div>
+              <Switch>
+              <Route exact path="/" component={Newfeed}/>
+              </Switch>
+              </div>
+          </BrowserRouter>
         </div>
         
     );
