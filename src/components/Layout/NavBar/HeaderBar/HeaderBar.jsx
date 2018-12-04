6ef6769bf302e087b0a7710f4be70fcd73ba9211
Cris <duytruong0702@gmail.com>
2018-12-04 20:50:01 +0700

    Duy

--- src/components/Layout/NavBar/HeaderBar/HeaderBar.jsx
+++ src/components/Layout/NavBar/HeaderBar/HeaderBar.jsx
@@ -21,7 +21,7 @@ class HeaderBar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/"><i className="fa fa-home fa-1x"></i></a>
+                <a className="nav-link" href="/"><i className="fa fa-home fa-1x"></i>Home<span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#"><i className="fa fa-bell fa-1x"></i>Notification <span className="badge badge-primary ">4</span></a>
