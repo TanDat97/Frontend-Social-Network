403768234b1b38a275218db188144e8599368426
Vippergod123 <trantandat130497@gmail.com>
2018-12-04 22:24:23 +0700

    Dat - change headerbarstyle

--- src/components/Layout/NavBar/HeaderBar/HeaderBar.jsx
+++ src/components/Layout/NavBar/HeaderBar/HeaderBar.jsx
@@ -6,13 +6,11 @@ const avatarUser = {
   borderRadius: "50%"
 }
 
-
 class HeaderBar extends Component {
-
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white shadow sticky-top">
         <div className = "container">
           <a className="navbar-brand mb-0 h1" href="/">Forest</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,20 +22,17 @@ class HeaderBar extends Component {
                 <a className="nav-link" href="/"><i className="fa fa-home fa-1x"></i>Home<span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#"><i className="fa fa-bell fa-1x"></i>Notification <span className="badge badge-primary ">4</span></a>
+                <a className="nav-link" href="/notification"><i className="fa fa-bell fa-1x"></i>Notification <span className="badge badge-primary ">4</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#"><i className="fa fa-comments fa-1x"></i>Message <span className="badge badge-primary">4</span></a>
+                <a className="nav-link" href="/message"><i className="fa fa-comments fa-1x"></i>Message <span className="badge badge-primary">4</span></a>
               </li>
             </ul>
             <form className="form-inline my-2 my-lg-0">
               <input className="form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search"/>
-              
             </form>
-            
           </div>
           <a href = "/profile"><img alt="avatar" className="rounded-circle img-fluid" style={avatarUser}  src="https://znews-photo.zadn.vn/w1024/Uploaded/mdf_xqkxvu/2018_11_19/Lucern1.JPG"/></a>
-          
         </div>
       </nav>
     );
