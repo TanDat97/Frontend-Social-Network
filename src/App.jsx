2b794add6036dfb817ce024bd3c56d15d7496776
thanhdai261097 <nguyenthanhdai97@gmail.com>
2018-12-09 21:14:32 +0700

    fix signin

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,29 @@ import HomePage from './Components/Profile/Profile'
 import Following from './Components/Follow/Following/Followings';
 import Follower from './Components/Follow/Follower/Follwers';
 import Signin from './Components/Profile/Auth/Signin'
-
+import Signup from './Components/Profile/Auth/Signup'
 
 class App extends Component {
   render() {
     return (
-        <div>
-          <HeaderBar/>
-          <br/>
+        
+          
+         
           <BrowserRouter >
               <div className = "container">
+              <HeaderBar/>
+              <br/>
               <Switch>
               <Route exact path="/" component={Newfeed}/>
               <Route exact path = "/profile" component ={HomePage}/>
               <Route exact path = "/signin" component = {Signin}/>
-                <Route exact path="/:id/following" component={Following}/>
-                <Route exact path="/:id/follower" component={Follower}/>
+              <Route path = "/signup" component = {Signup}/>
+              <Route exact path="/:id/following" component={Following}/>
+              <Route exact path="/:id/follower" component={Follower}/>
               </Switch>
               </div>
           </BrowserRouter>
-        </div>
+      
     );
   }
 }
--- src/Components/Layout/NavBar/HeaderBar/Link/SignoutLink.jsx
+++ src/Components/Layout/NavBar/HeaderBar/Link/SignoutLink.jsx
@@ -4,11 +4,22 @@ import { NavLink } from 'react-router-dom'
 
 const SignoutLink = () => { 
     return (
-        <ul className = "right">
-            <li> <NavLink to = "/signup">Sign Up</NavLink> </li>
-            <li> <NavLink to = "/signin">Sign In</NavLink> </li>
+        
+        <div className="btn-group cover-menu-mobile hidden-lg hidden-md">
+        <button type="button" className="btn btn-theme btn-sm dropdown-toggle" data-toggle="dropdown">
+            <i className="fa fa-bars"></i>
+        </button>
+        <ul className="dropdown-menu pull-right no-border" role="menu">
+           
             
+            
+            <li><a href="/signup" ><i className="fa fa-fw fa-users"></i><span> Sign Up</span></a></li>
+            <li><a href="/signin" ><i className="fa fa-fw fa-users"></i><span> Sign In</span></a></li>
+            
+           
         </ul>
+    </div>
+        
     )
 }
 
