947154376ad3dea6b4357d131ca17687becc10b6
thanhdai261097 <nguyenthanhdai97@gmail.com>
2018-12-09 17:08:26 +0700

    Update src/Components/Newsfeed/Newsfeed.jsx

--- src/Components/Newsfeed/Newsfeed.jsx
+++ src/Components/Newsfeed/Newsfeed.jsx
@@ -141,7 +141,7 @@ class Newfeed extends Component {
                            <br/>
                             <Form >
                                 <FormGroup controlId="formControlsTextarea">
-                                <textarea name="message-to-send" id="message-to-send" placeholder="Type your message" rows="3" name = "tag" ref= "tags"></textarea>
+                                <input className = "form-control" name="message-to-send" id="message-to-send" placeholder="Type your message" rows="3" name = "tag" ref= "tags"></input>
                                 </FormGroup>
                                 
                                 <button  onClick = {()=>this.handleComments(each)} className ="float-right">Post</button>
