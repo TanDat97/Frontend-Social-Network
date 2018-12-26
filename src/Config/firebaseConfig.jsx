7e3a5a60334fd4cd2379ce831a5cb5fd1e7ba118
Vippergod123 <trantandat130497@gmail.com>
2018-12-27 01:12:15 +0700

    fix bug

--- src/Config/firebaseConfig.jsx
+++ src/Config/firebaseConfig.jsx
@@ -4,6 +4,15 @@ import 'firebase/firebase-auth'
 import 'firebase/firebase-database'
 import 'firebase/firebase-storage'
 
+
+// var config = {
+//   apiKey: "AIzaSyCNn-6ZIpbpYwwcorr_TnxgLaktmuFzBek",
+//   authDomain: "blockchain-3752f.firebaseapp.com",
+//   databaseURL: "https://blockchain-3752f.firebaseio.com",
+//   projectId: "blockchain-3752f",
+//   storageBucket: "blockchain-3752f.appspot.com",
+//   messagingSenderId: "400603133507"
+// };
 var config = {
   apiKey: "AIzaSyCE_kWJ9eAidrbvtUE263Ciyx076UqsUtk",
   authDomain: "blockchain-5f681.firebaseapp.com",
