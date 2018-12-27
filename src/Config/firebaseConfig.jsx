205952fc7f09ea3c50892cd4a400b18c6ca7f340
Vippergod123 <trantandat130497@gmail.com>
2018-12-27 12:49:43 +0700

    firebase

--- src/Config/firebaseConfig.jsx
+++ src/Config/firebaseConfig.jsx
@@ -5,7 +5,7 @@ import 'firebase/firebase-auth'
 import 'firebase/firebase-database'
 import 'firebase/firebase-storage'
 
-var config = {
+var config = { // thanh dai
   apiKey: "AIzaSyCNn-6ZIpbpYwwcorr_TnxgLaktmuFzBek",
   authDomain: "blockchain-3752f.firebaseapp.com",
   databaseURL: "https://blockchain-3752f.firebaseio.com",
@@ -14,6 +14,14 @@ var config = {
   messagingSenderId: "400603133507"
 };
 
+// var config = { // si duy
+//   apiKey: "AIzaSyCE_kWJ9eAidrbvtUE263Ciyx076UqsUtk",
+//   authDomain: "blockchain-5f681.firebaseapp.com",
+//   databaseURL: "https://blockchain-5f681.firebaseio.com",
+//   projectId: "blockchain-5f681",
+//   storageBucket: "blockchain-5f681.appspot.com",
+//   messagingSenderId: "632514206473"
+// };
 
 firebase.initializeApp(config);
 firebase.firestore().settings({timestampsInSnapshots:true});
